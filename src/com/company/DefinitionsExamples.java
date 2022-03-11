23e4794cdbd9abf96f23225897bd9230e7182a09
Karri Wright <karriwright@Karris-MacBook-Pro.local>
2022-03-11 15:48:54 -0500

    Add initial repo

---
+++ src/com/company/DefinitionsExamples.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class DefinitionsExamples {
+    /*  VARIABLES are containers for storing data values
+            example:   int numOne;                   int is the variable type, assigning a name is declaing the variable;
+            example:  String name = "julie";         String is the variable type, declare the variable with the name and assign the variable by giving it the name julie.
+            example:  int total = numOne+ numTwo;    this is called inline initialization (provided a type, declared with name of total and assigned
+                                                     (other variables) all in one line.)
+}
+         METHODS    A method is a block of code which only runds when it is called.
+                    You can pass data, known as parameters/arguments, into a method.
+                    Methods are used to perform certain actions, and they are also known as funtions.
+                    Why use methods? to reuse code: define the code once and use it many times.  (Call upon the method using setters/getters)
+            example: public int addNumbers(int numOne, int numTwo) {
+                          int total = numOne + numTwo;
+                          return total;
+            example defined:    public=access modifier / int = return type/ addNumbers = method name /int numOne int num Two are the parameters
+                                    int total = numOne + numTwo; is the local variable  and is part of the method body.
+                                    return total is the return statement.
+            calling a method by usng  method name addNumbers(1,2) and adding numbers for the unknown variables.
+
+       FOR LOOPS
+            example  for/keyword/(int index = 0/initializer/; index <3/condition/; index++/incrementor/decrementor/) {
+                      System.out.println(index)
+
+       WHILE LOOPS
+            example  int index = 0           known as the initializer
+                     while (index <3)        known as the condition
+                        System.out.println(index);  known as the body
+                        i==                  known as the incrementor/decrementor
+
+       IF AND IF ELSE
+            example if(numOne >numTwo) {                     if is the keyword  (in parenthesis is the condition) {
+                    System.out.println(numOne + "is greater than" + numTwo   and the body telling ti what to do.
+
+            example  if(numOne >numTwo) {                     if is the keyword  (in parenthesis is the condition) {
+                    System.out.println(numOne + "is greater than" + numTwo   and the body telling it what to do.
+                    }else {
+                    System.out.println(numOne + "is less than" + numTwo   again the else statement body telling it what to do.
+     */
